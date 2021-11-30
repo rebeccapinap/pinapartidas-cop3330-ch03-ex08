63893ccc4aaea352e0a5c99375dc97230b9ee3d5
Rebecca Pina Partidas <rebecca.pinap@gmail.com>
2021-11-29 20:48:00 -0500

    Second Commit

--- pinapartidas-cop3330-ch03-ex08.cpp
+++ pinapartidas-cop3330-ch03-ex08.cpp
@@ -1,11 +1,15 @@
+// Including the header file
 #include "header.h"
 
 int main() 
 {
     int num = 0;
+
+    // Asking for user input
     cout << "Please enter a number: \n"; 
     cin >> num;
 
+    // Output
     if (num % 2 == 0)
         cout << "The value " << num << " is an even number.\n";
     else
